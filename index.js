f2349cfb49e23307125c79a41696ca9b07fb99de
badstar6733 <woranee87190@gmail.com>
2025-07-13 01:14:23 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ async function test() {
   const browser = await puppeteer.launch({
     headless: true,
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
-    // ลบ executablePath ทิ้งไป เพราะ puppeteer ตัวเต็มจะหาเอง
-    // executablePath: puppeteer.executablePath(),
   });
   const page = await browser.newPage();
   await page.goto("https://example.com");
